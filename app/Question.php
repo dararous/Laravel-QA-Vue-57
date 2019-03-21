f6676fc336f9870c03421644a3f63bd95ab2bf60
RousChandara <dara.chandara.chan@gmail.com>
2019-03-21 15:08:50 +0700

    First comit

---
+++ app/Question.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    protected $fillable = ['title', 'body'];
+
+    public function user() {
+        return $this->belongsTo(User::class); // One to Many
+    }
+
+    public function answers () {
+        return $this->hasMany(Answer::class);
+    }
+
+    // create slug format
+    public function setTitleAttribute($value) {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
+    }
+
+    public function getUrlAttribute() {
+        return route("questions.show", $this->slug);
+    }
+
+    public function getCreatedDateAttribute() {
+        return $this->created_at->diffForHumans(); // format 1hour ago
+    }
+
+    public function getStatusAttribute() {
+        if ($this->answers_count > 0) {
+            if ($this->best_answer_id) {
+                return "answered-accepted";
+            }
+            return 'answered';
+        }
+        return "unanswered";
+    }
+
+    public function getBodyHtmlAttribute() {
+        return \Parsedown::instance()->text($this->body);
+    }
+
+    public function acceptBestAnswer(Answer $answer) {
+        $this->best_answer_id = $answer->id;
+        $this->save();
+    }
+
+    public function favorites()
+    {
+        return $this->belongsToMany(User::class, 'favorites')->withTimestamps(); //, 'question_id', 'user_id');
+    }
+
+    public function isFavorited()
+    {
+        return $this->favorites()->where('user_id', auth()->id())->count() > 0;
+    }
+
+    public function getIsFavoritedAttribute()
+    {
+        return $this->isFavorited();
+    }
+
+    public function getFavoritesCountAttribute()
+    {
+        return $this->favorites->count();
+    }
+
+    public function votes()
+    {
+        return $this->morphedByMany(User::class, 'votable');
+    }
+
+    public function upVotes()
+    {
+        return $this->votes()->wherePivot('vote', 1);
+    }
+    public function downVotes()
+    {
+        return $this->votes()->wherePivot('vote', -1);
+    }
+}
---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        // create question by user
+//        factory(App\User::class, 3)->create()->each(function ($u) {
+//            $u->questions()
+//                ->saveMany(
+//                    factory(App\Question::class, rand(1, 5))->make()
+//                )
+//                ->each(function ($q) {
+//                    $q->answers()->saveMany(
+//                       factory(App\Answer::class, rand(1,5))->make());
+//                });
+//        });
+//        factory(\App\Question::class, 10)->create();
+
+        $this->call([
+            UsersQuestionsAnswersTableSeeder::class,
+            FavoritesTableSeeder::class,
+            VotablesTableSeeder::class,
+        ]);
+    }
+}
